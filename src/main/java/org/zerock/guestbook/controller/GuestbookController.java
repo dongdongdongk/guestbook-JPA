4369d17aaf43b313d40e6989b41b7bbb61821029
dongdongdongk <dhk9309@gmail.com>
2023-11-06 15:36:40 +0900

    Initial commit

---
+++ src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -0,0 +1,16 @@
+package org.zerock.guestbook.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/guestbook")
+@Log4j2
+public class GuestbookController {
+
+    public String list() {
+        log.info("list...........");
+        return "/guestbook/list";
+    }
+}
