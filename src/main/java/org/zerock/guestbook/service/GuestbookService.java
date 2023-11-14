024b228790567addcb8586517121254271168c38
dongdongdongk <dhk9309@gmail.com>
2023-11-15 07:32:10 +0900

    검색 기능

--- src/main/java/org/zerock/guestbook/service/GuestbookService.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -45,42 +45,4 @@ public interface GuestbookService {
         return dto;
     }
 
-    private BooleanBuilder getSearch(PageRequestDTO requestDTO) {
-
-        String type = requestDTO.getType(); // 타입 정보 얻어오기 (제목,작성자,내용)
-
-        BooleanBuilder booleanBuilder = new BooleanBuilder(); // 불리언 빌더 생성
-
-        if(type == null || type.trim().length() == 0) { // 검색 조건이 없는 경우
-            return booleanBuilder;
-        }
-
-        QGuestbook qGuestbook = QGuestbook.guestbook; // Q도메인 객체
-
-        String keyword = requestDTO.getKeyword(); // 키워드 받아오기
-
-        BooleanExpression expression = qGuestbook.gno.gt(0L); // gno > 0 조건만 생성
-
-        booleanBuilder.and(expression);
-
-
-        // 검색 조건 작성하기
-        BooleanBuilder conditionBuilder = new BooleanBuilder();
-
-        if (type.contains("t")) {
-            conditionBuilder.or(qGuestbook.title.contains(keyword));
-        }
-        if (type.contains("c")) {
-            conditionBuilder.or(qGuestbook.content.contains(keyword));
-        }
-        if (type.contains("w")) {
-            conditionBuilder.or(qGuestbook.writer.contains(keyword));
-        }
-
-        // 모든 조건 통합
-        booleanBuilder.and(conditionBuilder);
-
-        return booleanBuilder;
-    }
-
 }
--- src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,5 +1,7 @@
 package org.zerock.guestbook.service;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
+import org.zerock.guestbook.entity.QGuestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
 
 import java.util.Optional;
@@ -69,11 +72,55 @@ public class GuestbookServiceImpl implements GuestbookService {
     public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
         Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
 
-        Page<Guestbook> result = repository.findAll(pageable);
+        BooleanBuilder booleanBuilder = getSearch(requestDTO); // 검색 조건 처리 getSearch 를 하면 BooleanBuilder 가 나올거고
+
+        Page<Guestbook> result = repository.findAll(booleanBuilder,pageable); //findAll 을 할 때 booleanBuilder 을 이용해서 Querydsl 사용
+
+//        Page<Guestbook> result = repository.findAll(pageable); // 과거 버전
 
         Function<Guestbook, GuestbookDTO> fn = (entity ->
                 entityToDto(entity));
 
         return new PageResultDTO<>(result, fn);
+    }
+
+
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO) { // BooleanBuilder 를 리턴 해준다
+
+        //타입 조건 처리
+        String type = requestDTO.getType(); // 타입 정보 얻어오기 (제목,작성자,내용)
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder(); // 불리언 빌더 생성
+
+        if(type == null || type.trim().length() == 0) { // 검색 조건(타입)이 없는 경우
+            return booleanBuilder;
+        }
+
+        QGuestbook qGuestbook = QGuestbook.guestbook; // Q도메인 객체
+
+        String keyword = requestDTO.getKeyword(); // 키워드 받아오기
+
+        BooleanExpression expression = qGuestbook.gno.gt(0L); // gno > 0 조건만 생성
+
+        booleanBuilder.and(expression); // and 로 추가
+
+
+        // 검색 조건 작성 (or or 로 추가해서 작성)
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+        if (type.contains("t")) {
+            conditionBuilder.or(qGuestbook.title.contains(keyword)); // or or 로 계속 추가중
+        }
+        if (type.contains("c")) {
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+        if (type.contains("w")) {
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+
+        // 모든 조건 통합 ( conditionBuilder 가 booleanBuilder 안에 담기고 위의  .and(expression)(검색 타입) 가 추가된다 )
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
     }
 }
